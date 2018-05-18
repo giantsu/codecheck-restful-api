ccab3cb24bba289fe1c3b3d751ea266e3565a333
imura_s <imura_s@mti.co.jp>
2018-05-18 18:05:15 +0900

    no message

--- source/route/get.js
+++ source/route/get.js
@@ -1,8 +1,17 @@
 const handler = require("../handler");
+const DynamoDB = require("../lib/dynamodb");
 
 module.exports.get = handler(function*(event) {
   console.log(event);
+  const { Item } = yield DynamoDB.get({
+    TableName: "recipes",
+    Key: { id: parseInt(event.pathParameters.id) },
+  }).promise();
   return {
     statusCode: 200,
+    body: {
+      message: "Recipe details by id",
+      recipe: [Item],
+    },
   };
 });
