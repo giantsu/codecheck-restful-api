db5ff69142b944e77181eb31bf7f503481626934
imura_s <imura_s@mti.co.jp>
2018-05-18 18:40:55 +0900

    no message

--- source/route/remove.js
+++ source/route/remove.js
@@ -1,8 +1,22 @@
 const handler = require("../handler");
+const DynamoDB = require("../lib/dynamodb");
 
 module.exports.delete = handler(function*(event) {
-  console.log(event);
+  let resBody, resStatus;
+  try {
+    yield DynamoDB.delete({
+      TableName: "recipes",
+      Key: { id: parseInt(event.pathParameters.id) },
+    }).promise();
+    resStatus = 200;
+    resBody = { message: "Recipe successfully removed!" };
+  } catch (e) {
+    resStatus = 400;
+    resBody = { message: "No Recipe found" };
+  }
+
   return {
-    statusCode: 200,
+    statusCode: resStatus,
+    body: resBody,
   };
 });
